12899fe89ecfe62d85898c2185764ce7f1a85e07
28831810 <rynoconradie12@gmail.com>
2020-11-17 15:40:50 +0200

    test patch and delete

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { routes } = require('..');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
@@ -14,7 +15,7 @@ router.post('/', (req, res, next) => {
 })
 
 router.get('/:userId', (req, res, next) => {
-    const id = req.params.productId;
+    const id = req.params.userId;
     if(id == 'special'){
         res.status(200).json({
             message: 'you descovered special ID',
@@ -25,6 +26,18 @@ router.get('/:userId', (req, res, next) => {
             message: 'youre ID is',
         });
     }
+})
+
+routes.patch('/:usersId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Patched product'
+    });
+})
+
+routes.delete('/:usersId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Deleted product'
+    });
 })
 
 module.exports = router;
