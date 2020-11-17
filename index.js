4eb59e398dbf34ff8605288c6de081170be02a45
28831810 <rynoconradie12@gmail.com>
2020-11-17 14:49:25 +0200

    test commit

--- index.js
+++ index.js
@@ -10,15 +10,16 @@ const userRoutes = require('./routes/users');
 
 app.use('/users', userRoutes)
 
-// app.get("/test", function(request, response)
-// {
-//     response.json({ "hell please work" : "yeah it works" });
-// });
+app.get("/test", function(request, response)
+{
+    response.json({ "hell please work" : "yeah it works" });
+});
 
-// //Start the server
-// app.listen(port, function ()
-// {
-//     console.log("API server running " + "(Started " + new Date() + ")");
-// });
+//Start the server
+
+app.listen(port, function ()
+{
+    console.log("API server running " + "(Started " + new Date() + ")");
+});
 
 module.exports = app;
