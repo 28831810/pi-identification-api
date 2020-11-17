c463d6498c97ddb524a4a44a5c42447c37f33415
28831810 <rynoconradie12@gmail.com>
2020-11-17 13:04:42 +0200

    installed mongoose package

--- index.js
+++ index.js
@@ -5,6 +5,30 @@ var app = express();
 app.use(bodyParser.json());
 var port = process.env.PORT || 3838;
 
+// const mongoose = require('mongoose');
+
+
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://studentAdmin:admin@word1@pi-identification-clust.iktww.mongodb.net/<dbname>?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
+
+// app.post('/users', (req, res, next) => {
+//     const user = {
+//         name: req.body.name,
+//         surname: req.body.surname
+//     }
+//     res.status(201).json({
+//         message: 'Handling post request to /user',
+//         createUser: user
+//     })
+// })
+
 app.get("/test", function(request, response)
 {
     response.json({ "hell please work" : "yeah it works" });
