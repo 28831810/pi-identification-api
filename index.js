637c574d611c117fb9c9d1e5b7c89c76cffbd47c
28831810 <rynoconradie12@gmail.com>
2020-11-17 16:23:39 +0200

    added a log file test

--- index.js
+++ index.js
@@ -5,9 +5,20 @@ var app = express();
 app.use(bodyParser.json());
 var port = process.env.PORT || 3838;
 
+const morgan = require('morgan');
 
 const userRoutes = require('./routes/users');
 const infoRoutes = require('./routes/info');
+
+//logger
+var logger = require('morgan');
+
+app.use(logger('common', {
+    stream: fs.createWriteStream('./access.log', {flags: 'a'})
+}));
+app.use(logger('dev'));
+
+//logger
 
 app.use('/users', userRoutes)
 app.use('/info', infoRoutes)
